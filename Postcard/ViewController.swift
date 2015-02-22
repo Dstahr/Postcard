79de75d661b3275ae3d15e0db2701e7dd63a43fe
David Stahr <brightstar9007@Davids-MBP-7.home>
2015-02-21 23:06:39 -0500

    comment test

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //comnet to test git hub. 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLable.hidden = false
         messageLable.text = enterMessageTextField.text
